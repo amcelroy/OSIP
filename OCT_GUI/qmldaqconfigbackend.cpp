9312520ee8787ff60a49ca51636d06230762b956
amcelroy <mcelroy.austin@gmail.com>
2017-10-24 16:48:26 -0500

    Added a QML based DAQ Config window Added a test project for the QML Daq Config Window

---
+++ OCT_GUI/qmldaqconfigbackend.cpp
@@ -0,0 +1,6 @@
+#include "qmldaqconfigbackend.h"
+
+QMLDAQConfigBackend::QMLDAQConfigBackend()
+{
+
+}
---
+++ OCT_GUI/qmldaqconfigbackend.h
@@ -0,0 +1,12 @@
+#ifndef QMLDAQCONFIGBACKEND_H
+#define QMLDAQCONFIGBACKEND_H
+
+#include <QObject>
+
+class QMLDAQConfigBackend
+{
+public:
+    QMLDAQConfigBackend();
+};
+
+#endif // QMLDAQCONFIGBACKEND_H
